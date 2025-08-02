b20508692ab1bf713e1a6e4eca5a0eea354a5282
fosterbarnes <barnesfostert@gmail.com>
2025-08-02 04:36:48 -0400

    Updated to include Anus (Uranus -> Anus)

--- GayPenis/GayPenis.lua
+++ GayPenis/GayPenis.lua
@@ -2,7 +2,7 @@
 --- MOD_NAME: GayPenis
 --- MOD_ID: GayPenis
 --- MOD_AUTHOR: [FosterBarnes, THEGODBOYYT]
---- MOD_DESCRIPTION: Turns Straights -> Gay and Venus -> Penis
+--- MOD_DESCRIPTION: Turns Straights -> Gay and Venus -> Penis (Now with Anus)
 --- BADGE_COLOR: FF46A2
 --- DISPLAY_NAME: Hehe, Penis
 
@@ -42,9 +42,19 @@ function SMODS.INIT.GayPenis()
         px = 71,
         py = 95,
     }
+    SMODS.Atlas {
+        key = 'Anus',
+        path = 'Tarots.png',
+        px = 71,
+        py = 95,
+    }
     SMODS.Consumable:take_ownership('c_venus', {
         atlas = 'Penis', 
         pos = {x=1,y=3},
+    })
+    SMODS.Consumable:take_ownership('c_uranus', {
+        atlas = 'Anus', 
+        pos = {x=6,y=3},
     })
 
     -- Modify localization for poker hands
@@ -52,6 +62,7 @@ function SMODS.INIT.GayPenis()
         G.localization.misc.poker_hands['Straight Flush'] = "Gay Flush"
         G.localization.misc.poker_hands['Straight'] = "Gay"
         G.localization.descriptions.Planet.c_venus.name = "Penis"
+        G.localization.descriptions.Planet.c_uranus.name = "Anus"
         sendDebugMessage("Homosexuality complete.")
     else
         sendDebugMessage("Homophobia has blocked this Gay Penis... smh")
